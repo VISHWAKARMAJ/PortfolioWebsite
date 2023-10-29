be82c8d061dea50314b227120e1fe99e0f22a697
PANKAJ KUMAR VISHWAKARMA <92736385+VISHWAKARMAJ@users.noreply.github.com>
2023-10-29 09:40:47 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+    <div style={{fontWeight:"bold"}}> Pankaj Vishwakarma</div>
+    <div style={{fontWeight:"bold"}}> Project</div>
+    </div>
+  );
+}
